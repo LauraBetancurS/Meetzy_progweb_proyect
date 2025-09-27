32b42f87ed7edd29896ede3a956680d0b148ea64
Daniela Rojas <drojasaristi19@gmail.com>
2025-09-27 18:53:28 -0500

    chore: update routes app security

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Layout from './components/Layout'
-import Dashboard from './pages/Dashboard'
-import Comunidades from './pages/Comunidades'
-import UnirteComunidad from './pages/UnirteComunidad'
-import CrearComunidad from './pages/CrearComunidad'
-import Perfil from './pages/Perfil'
-import CrearEvento from './pages/CrearEvento'
-import CuestionarioMood from './pages/CuestionarioMood'
-import Eventos from './pages/Eventos'
-import NotFound from './pages/NotFound'
+import { Router } from './Router'
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="comunidades" element={<Comunidades />} />
-        <Route path="unirte-comunidad" element={<UnirteComunidad />} />
-        <Route path="crear-comunidad" element={<CrearComunidad />} />
-        <Route path="perfil" element={<Perfil />} />
-        <Route path="crear-evento" element={<CrearEvento />} />
-        <Route path="cuestionario-mood" element={<CuestionarioMood />} />
-        <Route path="eventos" element={<Eventos />} />
-        <Route path="404" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
-      </Route>
-    </Routes>
+    <Router/>
   )
 }
