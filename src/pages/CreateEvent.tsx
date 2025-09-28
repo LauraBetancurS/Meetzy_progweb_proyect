8a5949e048311a96fc4b8f610f57bb59bebb81bd
LauraBetancurS <lau1betancur1@gmail.com>
2025-09-28 17:05:26 -0500

    chore:create event page

--- src/pages/CreateEvent.tsx
+++ src/pages/CreateEvent.tsx
@@ -1,3 +1,4 @@
+// src/pages/CreateEvent.tsx
 import React from "react";
 import "./CreateEvent.css";
 import { EventForm } from "../components/EventForm/EventForm";
@@ -10,25 +11,26 @@ const CreateEventPage: React.FC = () => {
 
   return (
     <div className="createEvent__wrap">
-      {/* LEFT: mock image (static for now) */}
+      {/* LEFT: imagen mock */}
       <section className="createEvent__imageCard">
         <div className="mockImage__frame">
-          {/* Replace src with your asset if you have one in /public */}
           <img
-            src="https://images.unsplash.com/photo-1511578314322-379afb476865?q=80&w=1200&auto=format&fit=crop"
+            src="https://i.pinimg.com/736x/c9/06/bb/c906bbea85c5370a3ea2e056c4bae277.jpg"
             alt="Mock del evento"
           />
           <span className="mockImage__badge">imagen del evento aquí</span>
         </div>
       </section>
 
-      {/* RIGHT: styled form */}
+      {/* RIGHT: formulario */}
       <section className="createEvent__formCard">
-        <h1>Crear evento</h1>
         <EventForm
           onCreate={(data) => {
             const created = addEvent(data);
-            navigate("/events", { replace: false, state: { highlightId: created.id } });
+            navigate("/events", {
+              replace: false,
+              state: { highlightId: created.id },
+            });
           }}
         />
       </section>
