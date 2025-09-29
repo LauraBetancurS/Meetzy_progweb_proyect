02f57e704c0b24723070099a4409a6fd28c41d37
LauraBetancurS <lau1betancur1@gmail.com>
2025-09-29 13:45:48 -0500

    chore: simplify event creation logic

--- src/pages/CreateEvent.tsx
+++ src/pages/CreateEvent.tsx
@@ -1,55 +1,41 @@
 // src/pages/CreateEvent.tsx
-import React from "react";
-import "./CreateEvent.css";
-import { EventForm } from "../components/EventForm/EventForm";
-import { useEvents } from "../context/EventContext";
 import { useNavigate } from "react-router-dom";
+import { useEvents } from "../context/EventContext";
+import { EventForm } from "../components/EventForm/EventForm";
+import "./CreateEvent.css";
 
-const FALLBACK_IMG =
-  "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1200&auto=format&fit=crop";
 
-const CreateEventPage: React.FC = () => {
+
+export default function CreateEventPage() {
   const { addEvent } = useEvents();
   const navigate = useNavigate();
 
+  function handleCreate(data: Parameters<typeof addEvent>[0]) {
+    const created = addEvent(data);
+    navigate("/events", { state: { highlightId: created.id } });
+  }
+
   return (
     <div className="createEventPage">
-
-      {/* Main content */}
-      <div className="createEventPage__content" role="main">
+      <div className="createEventPage__content">
         <div className="createEvent__wrap">
-          {/* LEFT: mock image */}
+          {/* Izquierda: imagen de referencia */}
           <section className="createEvent__imageCard" aria-label="Event preview">
             <div className="mockImage__frame">
               <img
-                src={
-                  "https://i.pinimg.com/736x/c9/06/bb/c906bbea85c5370a3ea2e056c4bae277.jpg" ||
-                  FALLBACK_IMG
-                }
+                src="https://i.pinimg.com/736x/c9/06/bb/c906bbea85c5370a3ea2e056c4bae277.jpg"
                 alt="Event mock preview"
               />
               <span className="mockImage__badge">imagen del evento aquí</span>
             </div>
           </section>
 
-          {/* RIGHT: form */}
+          {/* Derecha: formulario */}
           <section className="createEvent__formCard">
-            
-
-            <EventForm
-              onCreate={(data) => {
-                const created = addEvent(data);
-                navigate("/events", {
-                  replace: false,
-                  state: { highlightId: created.id },
-                });
-              }}
-            />
+            <EventForm onCreate={handleCreate} />
           </section>
         </div>
       </div>
     </div>
   );
-};
-
-export default CreateEventPage;
+}
