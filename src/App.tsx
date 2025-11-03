8169c9fae851148c2ce241e2ea0a504cfafd1525
Daniela Rojas <drojasaristi19@gmail.com>
2025-11-03 14:57:23 -0500

    refactor: update main app structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import { Router } from './Router'
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Layout
+import Layout from "./components/Layout";
+
+// Pages
+import Dashboard from "./pages/Dashboard";
+import EventsPage from "./pages/Events";
+import CreateEventPage from "./pages/CreateEvent";
+import Comunidades from "./pages/Comunidades";
+import CuestionarioMood from "./pages/CuestionarioMood";
+import Perfil from "./pages/Perfil";
+import { Login } from "./pages/Login";
+import { RegisterPage } from "./pages/Register";
 
 export default function App() {
   return (
-    <Router/>
-  )
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/events/new" element={<CreateEventPage />} />
+          <Route path="/comunidades" element={<Comunidades />} />
+          <Route path="/mood" element={<CuestionarioMood />} />
+          <Route path="/perfil" element={<Perfil />} />
+        </Route>
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
 }
