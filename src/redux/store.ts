cabde464e10a947bf5e2f3013ed1df6fa1ed0b2a
Daniela Rojas <drojasaristi19@gmail.com>
2025-11-03 14:59:38 -0500

    feat: initialize redux store configuration

---
+++ src/redux/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import appReducer from "./slices/AppSlice";
+import authReducer from "./slices/AuthSlice";
+import eventsReducer from "./slices/EventsSlice";
+import subscriptionsReducer from "./slices/SubscriptionsSlice";
+
+export const store = configureStore({
+  reducer: {
+    app: appReducer,
+    auth: authReducer,
+    events: eventsReducer,
+    subscriptions: subscriptionsReducer,
+  },
+});
+
+// tipos para usar en toda la app
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
