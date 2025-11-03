7bf251e1072c1c7bbc04f52387f8dae37a5e835f
Daniela Rojas <drojasaristi19@gmail.com>
2025-11-03 14:59:11 -0500

    feat: update profile page and user info display

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -1,13 +1,18 @@
-import { useApp } from '../context/AppContext'
-import { DevNavButtons } from './DevNavButtons'
+import { useAppSelector } from "../redux/hooks";
+import { DevNavButtons } from "./DevNavButtons";
 
 export default function Perfil() {
-  const { user } = useApp()
+  const user = useAppSelector((state) => state.app.user);
+
   return (
     <section>
       <h1>Perfil</h1>
-      {user ? (<pre>{JSON.stringify(user, null, 2)}</pre>) : (<p>Sin sesión</p>)}
+      {user ? (
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+      ) : (
+        <p>Sin sesión</p>
+      )}
       <DevNavButtons />
     </section>
-  )
+  );
 }
