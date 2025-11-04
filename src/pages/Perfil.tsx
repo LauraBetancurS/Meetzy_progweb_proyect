34bb9a108aa4f9e10bacfdc10bd41f438f22aade
LauraBetancurS <lau1betancur1@gmail.com>
2025-11-04 18:24:04 -0500

    feat: implement Perfil page showing user data and avatar

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -1,18 +1,74 @@
-import { useAppSelector } from "../redux/hooks";
-import { DevNavButtons } from "./DevNavButtons";
+// src/pages/Perfil.tsx
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { fetchMyProfile } from "../redux/slices/ProfileSlice";
+import ProfileHeader from "../components/ProfileHeader";
+import "./Perfil.css";
 
 export default function Perfil() {
-  const user = useAppSelector((state) => state.app.user);
+  const dispatch = useAppDispatch();
+  const { me, isLoading, error } = useAppSelector((s) => s.profile);
+  const { user } = useAppSelector((s) => s.auth);
+
+  useEffect(() => {
+    // only try to load profile if we have an auth user
+    if (user) dispatch(fetchMyProfile());
+  }, [dispatch, user]);
+
+  if (!user) {
+    return (
+      <div className="perfil-wrap">
+        <p className="perfil-info">Debes iniciar sesión para ver tu perfil.</p>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="perfil-wrap">
+        <p className="perfil-info">Cargando perfil…</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="perfil-wrap">
+        <p className="perfil-error">Error: {error}</p>
+      </div>
+    );
+  }
 
   return (
-    <section>
-      <h1>Perfil</h1>
-      {user ? (
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-      ) : (
-        <p>Sin sesión</p>
-      )}
-      <DevNavButtons />
-    </section>
+    <div className="perfil-wrap">
+      <ProfileHeader
+        fullName={me?.full_name}
+        userName={me?.user_name}
+        avatarUrl={me?.avatar_url}
+        tagline={me?.tagline || null}
+      />
+
+      {/* Below you could list Events / Communities of this user */}
+      <div className="perfil-list">
+        {/* placeholder rows */}
+        <div className="perfil-list__item">
+          <div className="perfil-list__thumb" />
+          <div className="perfil-list__title">Out of the world</div>
+          <div className="perfil-list__pill" />
+        </div>
+
+        <div className="perfil-list__item">
+          <div className="perfil-list__thumb" />
+          <div className="perfil-list__title">Brunch con Runners</div>
+          <div className="perfil-list__pill" />
+        </div>
+
+        <div className="perfil-list__item">
+          <div className="perfil-list__thumb" />
+          <div className="perfil-list__title">Puppy Yoga</div>
+          <div className="perfil-list__pill" />
+        </div>
+      </div>
+    </div>
   );
 }
