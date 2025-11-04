59580799ab28ee98b58d805f3bcdc7ac340c5e59
LauraBetancurS <lau1betancur1@gmail.com>
2025-11-04 18:09:54 -0500

    feat: update App component structure and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
+// src/App.tsx
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Layout
 import Layout from "./components/Layout";
 
-// Pages
+// Pages (públicas)
+import { Login } from "./pages/Login";
+import { RegisterPage } from "./pages/Register";
+
+// Pages (privadas)
 import Dashboard from "./pages/Dashboard";
 import EventsPage from "./pages/Events";
 import CreateEventPage from "./pages/CreateEvent";
 import Comunidades from "./pages/Comunidades";
 import CuestionarioMood from "./pages/CuestionarioMood";
 import Perfil from "./pages/Perfil";
-import { Login } from "./pages/Login";
-import { RegisterPage } from "./pages/Register";
+
+// Auth sync + ProtectedRoute
+import { useSupabaseAuthSync } from "./hooks/useSupabaseAuthSync";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
+  // Mantiene Redux sincronizado con Supabase (sesión)
+  useSupabaseAuthSync();
+
   return (
     <Router>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/events/new" element={<CreateEventPage />} />
-          <Route path="/comunidades" element={<Comunidades />} />
-          <Route path="/mood" element={<CuestionarioMood />} />
-          <Route path="/perfil" element={<Perfil />} />
-        </Route>
-
+        {/* Rutas públicas */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegisterPage />} />
+
+        {/* Rutas privadas (requieren sesión) */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/events" element={<EventsPage />} />
+            <Route path="/events/new" element={<CreateEventPage />} />
+            <Route path="/comunidades" element={<Comunidades />} />
+            <Route path="/mood" element={<CuestionarioMood />} />
+            <Route path="/perfil" element={<Perfil />} />
+          </Route>
+        </Route>
       </Routes>
     </Router>
   );
