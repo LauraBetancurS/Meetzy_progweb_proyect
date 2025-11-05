705fad0a150cf386bdcfddabc59c854a44515be7
LauraBetancurS <lau1betancur1@gmail.com>
2025-11-04 20:08:19 -0500

    Chore: fix persit dispatch of auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+// src/main.tsx
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
 import "./index.css";
 
+// ✅ use the typed dispatch hook
+import { useAppDispatch } from "./redux/hooks";
+
+// your thunks/helpers
+import { initAuthFromSupabase, startAuthListener } from "./redux/slices/AuthSlice";
+
+function AuthBootstrap() {
+  const dispatch = useAppDispatch(); // <-- typed to AppDispatch
+
+  useEffect(() => {
+    dispatch(initAuthFromSupabase());        // no TS error now ✅
+    const unsubscribe = startAuthListener(dispatch);
+    return () => unsubscribe?.();
+  }, [dispatch]);
+
+  return <App />;
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <AuthBootstrap />
     </Provider>
   </React.StrictMode>
 );
