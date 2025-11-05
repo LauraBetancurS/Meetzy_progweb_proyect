814e2393d48335c86679ac1de8eec364a930204e
LauraBetancurS <lau1betancur1@gmail.com>
2025-11-05 00:41:27 -0500

    chore: create dislexic toogle

--- src/redux/slices/a11ySlice.ts
+++ src/redux/slices/a11ySlice.ts
@@ -1,16 +1,19 @@
+// src/redux/slices/a11ySlice.ts
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 export interface A11yState {
-  fontScale: number; // more flexible than fixed union
+  fontScale: number;
+  dyslexiaMode: boolean;
 }
 
 const initialState: A11yState = {
   fontScale: 1,
+  dyslexiaMode: false,
 };
 
-const MIN_SCALE = 0.6; // smaller than before
-const MAX_SCALE = 1.8; // bigger than before
-const STEP = 0.15; // each button press changes by 15%
+const MIN_SCALE = 0.6;
+const MAX_SCALE = 1.8;
+const STEP = 0.15;
 
 const a11ySlice = createSlice({
   name: "a11y",
@@ -29,11 +32,21 @@ const a11ySlice = createSlice({
     resetFont: (state) => {
       state.fontScale = 1;
     },
+    toggleDyslexiaMode: (state) => {
+      state.dyslexiaMode = !state.dyslexiaMode;
+    },
   },
 });
 
-export const { setFontScale, increaseFont, decreaseFont, resetFont } = a11ySlice.actions;
+export const {
+  setFontScale,
+  increaseFont,
+  decreaseFont,
+  resetFont,
+  toggleDyslexiaMode,
+} = a11ySlice.actions;
+
 export default a11ySlice.reducer;
 
-// Selector helper
 export const selectFontScale = (state: { a11y: A11yState }) => state.a11y.fontScale;
+export const selectDyslexiaMode = (state: { a11y: A11yState }) => state.a11y.dyslexiaMode;
