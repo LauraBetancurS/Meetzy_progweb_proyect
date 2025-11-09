e4012b0b808ea2512036d744816b5d438b5dd40d
LauraBetancurS <lau1betancur1@gmail.com>
2025-11-09 12:33:09 -0500

    chore(store):  PollSlice and update store structure

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,6 +6,7 @@ import subscriptionsReducer from "./slices/SubscriptionsSlice";
 import a11yReducer from "./slices/a11ySlice";
 import profileReducer from "./slices/ProfileSlice";
 import communitiesReducer from "./slices/CommunitiesSlice";
+import pollsReducer from './slices/PollsSlice';
 
 export const store = configureStore({
   reducer: {
@@ -16,6 +17,7 @@ export const store = configureStore({
     a11y: a11yReducer,    
     profile: profileReducer,        
     communities: communitiesReducer,
+    polls: pollsReducer,
   },
 });
 
