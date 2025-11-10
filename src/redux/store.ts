f8a1d84ba13cb161eb254bb37a80a89da6950ba0
Daniela Rojas <drojasaristi19@gmail.com>
2025-11-10 16:10:40 -0500

    refactor: update Redux store configuration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import appReducer from "./slices/AppSlice";
 import authReducer from "./slices/AuthSlice";
 import eventsReducer from "./slices/EventsSlice";
 import subscriptionsReducer from "./slices/SubscriptionsSlice";
@@ -11,7 +10,6 @@ import pollsReducer from './slices/PollsSlice';
 
 export const store = configureStore({
   reducer: {
-    app: appReducer,
     auth: authReducer,
     events: eventsReducer,
     subscriptions: subscriptionsReducer,
